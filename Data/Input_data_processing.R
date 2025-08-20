5eb5fcd7e7256ae6e6c9144c2eb6d7ddbeec7120
Greg Goodrum <A02290896@aggies.usu.edu>
2025-08-20 16:12:51 -0600

    DATA: Combine NABD and TU datasets into table for geospatial/connectivity processing

--- Data/Input_data_processing.R
+++ Data/Input_data_processing.R
@@ -42,7 +42,7 @@ setwd(pwd)
 # ---------------------------------------------------------------------
 
 # --------------------------------------------------------------------- #
-# 01. Barrier Data
+# 01. Barrier Data (currently only Bear Lake)
 # ---------------------------------------------------------------------
 
 # Summary: Combine NABD and TU barrier data into single dataset for analysis.
@@ -76,11 +76,73 @@ data.nabd.join <- data.nabd %>%
          Mitigated = Removed,
          YearMitigated = YearRemoved) %>%
   mutate(Source = 'National Aquatic Barrier Dataset (SARP)',
-         Pass_Before = Passability,
-         Pass_After = Passability) %>%
+         Pass_NABD = Passability) %>%
+  # Passability from ratings
+  mutate(Pass_Before = ifelse(Pass_NABD == 'Complete barrier', 0.0,
+                              ifelse(Pass_NABD == 'Unknown', NA,
+                                     ifelse(Pass_NABD %in% c('Partial passability', 'Partial passability - salmonid'),
+                                            0.5, 1.0)))) %>%
+  # Passability unknown - estimated from barrier type
+  mutate(Pass_Before = ifelse(Pass_NABD == 'Unknown' & BarrierType == 'Dam', 0,
+                              ifelse(Pass_NABD == 'Unknown' & BarrierType == 'Assessed road-related barrier',
+                                     0.5, Pass_Before))) %>%
+  mutate(Pass_After = Pass_Before) %>%
   select(BarrierName, BarrierType, StreamName, Source, SourceID,
          Lat, Lon,
-         Mitigated, YearMitigated, Pass_Before, Pass_After) %>%
+         Mitigated, YearMitigated, Pass_NABD, Pass_Before, Pass_After) %>%
   as.data.frame
 
+# ---------------------------------------------------------------------------- #
+
+# NOTE: Currently ignoring diversion screens, not sure these should be included in connectivity assessment.
+
+# Format TU Data - Culverts
+data.tu.culverts <- data.tu[['Culverts']] %>%
+  rename(BarrierName = `Culvert/Road Name`,
+         StreamName = `Waterway Name`,
+         Lat = `Culvert LAT`,
+         Lon = `Culvert LONG`,
+         YearMitigated = `Year Replaced`,
+         Pass_Before = `Culvert Passage before`,
+         Pass_After = `Culvert Passage after`) %>%
+  mutate(BarrierType = 'Culvert',
+         Source = 'Trout Unlimited (TU)',
+         SourceID = paste0('TU_CUL_', CUL_ID),
+         Mitigated = 'yes',
+         Pass_NABD = NA) %>%
+  select(BarrierName, BarrierType, StreamName, Source, SourceID,
+         Lat, Lon,
+         Mitigated, YearMitigated, Pass_NABD, Pass_Before, Pass_After) %>%
+  as.data.frame
+
+# Format TU Data - Diversions
+data.tu.diversions <- data.tu[['Diversions']] %>%
+  rename(BarrierName = `Div Name`,
+         StreamName = `Waterway Name`,
+         Lat = `Div LAT`,
+         Lon = `Div LONG`,
+         Mitigated = `Div struct Rebuild     (Y/N)`,
+         YearMitigated = `Div Rebuild Year      (YYYY)`,
+         Pass_Before = `Diversion Passability Before`,
+         Pass_After = `Diversion Passability After`) %>%
+  mutate(BarrierType = 'Diversion',
+         Source = 'Trout Unlimited (TU)',
+         SourceID = paste0('TU_DIV_', `Div #`),
+         Pass_NABD = NA,
+         Pass_After = as.numeric(Pass_After)) %>%
+  mutate(YearMitigated = as.numeric(YearMitigated)) %>%
+  select(BarrierName, BarrierType, StreamName, Source, SourceID,
+         Lat, Lon,
+         Mitigated, YearMitigated, Pass_NABD, Pass_Before, Pass_After) %>%
+  as.data.frame
+
+# ---------------------------------------------------------------------------- #
+
+# Combine and export datasets
+data.barriers <- do.call(rbind, list(data.nabd.join, data.tu.culverts, data.tu.diversions))
+
+# ---------------------------------------------------------------------------- #
+
+# Write output
+write.csv(data.barriers, file = 'Barriers_TU_NADB.csv', row.names = FALSE)
 # ---------------------------------------------------------------------
